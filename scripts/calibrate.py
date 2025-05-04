ed8ff12463d6a3e7172bd68250f005135dab1b70
qleml <cchristoph@student.ethz.ch>
2025-05-03 21:23:19 -0700

    api and changes, in the works

---
+++ scripts/calibrate.py
@@ -0,0 +1,7 @@
+from orca_core import OrcaHand, Retargeter
+
+hand = OrcaHand('/Users/ccc/dev/orca/orca_core/orca_core/models/orcahand_v1_berkeley')
+status = hand.connect()
+print(status)
+
+hand.calibrate()
