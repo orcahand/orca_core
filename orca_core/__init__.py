d116cb329879500a18966b5c1ea1f56399b090c5
Francesco Capuano <fc.francescocapuano@gmail.com>
2026-03-23 19:12:21 +0100

    minor

--- orca_core/__init__.py
+++ orca_core/__init__.py
@@ -5,6 +5,9 @@
 # You may use, copy, modify, and distribute this file under the terms of the MIT License.
 # See the LICENSE file at the root of this repository for full license information.
 # ==============================================================================
+from .calibration import CalibrationResult
 from .core import BaseHand
 from .core import MockOrcaHand
 from .core import OrcaHand
+from .core import OrcaJointPositions
+from .hand_config import HandConfig
